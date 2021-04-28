97b2c7a8a368e28a7b86829ab63c8574d7ee2719
Peter Hemsing <peterhemsing@gmail.com>
2021-04-28 13:17:27 +0200

    finishing the cart app

--- cart/src/reducer.js
+++ cart/src/reducer.js
@@ -1,14 +1,58 @@
-// const initialState = {
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CLEAR_CART":
+      return { ...state, cart: [] };
+    case "REMOVE":
+      return {
+        ...state,
+        cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+      };
+    case "INCREASE":
+      let tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount + 1 };
+        }
+        return cartItem;
+      });
+      return { ...state, cart: tempCart };
+    case "DECREASE":
+      let tempCart2 = state.cart
+        .map((cartItem) => {
+          if (cartItem.id === action.payload) {
+            return { ...cartItem, amount: cartItem.amount - 1 };
+          }
+          return cartItem;
+        })
+        .filter((cartItem) => cartItem.amount !== 0);
+      return { ...state, cart: tempCart2 };
+    case "GET_TOTALS":
+      let { total, amount } = state.cart.reduce(
+        (cartTotal, cartItem) => {
+          const { price, amount } = cartItem;
+          const itemTotal = price * amount;
 
-// }
+          cartTotal.total += itemTotal;
+          cartTotal.amount += amount;
+          return cartTotal;
+        },
+        {
+          total: 0,
+          amount: 0,
+        }
+      );
+      total = parseFloat(total.toFixed(2));
 
-// export default (state = initialState, { type, payload }) => {
-//     switch (type) {
+      return { ...state, total, amount };
 
-//     case typeName:
-//         return { ...state, ...payload }
+    case "LOADING":
+      return { ...state, loading: true };
 
-//     default:
-//         return state
-//     }
-// }
+    case "DISPLAY_ITEMS":
+      return { ...state, cart: action.payload, loading: false };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
