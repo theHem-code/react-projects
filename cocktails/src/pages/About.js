68688e762b9390790958e2cc4e0d54ff1051f98a
Peter Hemsing <peterhemsing@gmail.com>
2021-04-30 12:45:39 +0200

    finishing cocktail project

---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At dignissimos
+        soluta, porro, earum est quisquam odio magni numquam repudiandae id
+        obcaecati ipsa repellat neque eligendi suscipit, pariatur quia placeat
+        alias itaque impedit ipsam mollitia omnis. Animi ipsam repellat,
+        perferendis cupiditate placeat aut rerum dolores consequuntur, adipisci
+        quo sit deserunt earum. Incidunt nemo omnis mollitia assumenda impedit
+        officia tempore molestias quasi unde nesciunt, placeat nostrum
+        dignissimos, delectus modi. Fugiat facere dolor ad cupiditate ipsum
+        saepe repellendus cumque excepturi harum. Et ratione eos accusamus ea
+        veniam corrupti sapiente, obcaecati ex voluptate! Cum voluptatibus est
+        accusamus veniam dignissimos numquam, repudiandae praesentium possimus
+        autem?
+      </p>
+    </section>
+  );
+};
+
+export default About;
